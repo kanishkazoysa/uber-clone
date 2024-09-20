0cc360727b56fac1c405ab9b03dcbfefdfab783f
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-20 11:01:02 +0530

    edit tab bar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -32,64 +32,64 @@ export default function Layout() {
       initialRouteName="index"
       screenOptions={{
         tabBarActiveTintColor: "white",
-        // tabBarInactiveTintColor: "white",
-        // tabBarShowLabel: false,
-        // tabBarStyle: {
-        //   backgroundColor: "#333333",
-        //   borderRadius: 50,
-        //   paddingBottom: 0, // ios only
-        //   overflow: "hidden",
-        //   marginHorizontal: 20,
-        //   marginBottom: 20,
-        //   height: 78,
-        //   display: "flex",
-        //   justifyContent: "space-between",
-        //   alignItems: "center",
-        //   flexDirection: "row",
-        //   position: "absolute",
-        // },
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          paddingBottom: 0, // ios only
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          position: "absolute",
+        },
       }}
     >
       <Tabs.Screen
         name="home"
         options={{
           title: "Home",
-          headerShown: false,
+          // headerShown: false,
           tabBarIcon: ({ focused }) => (
             <TabIcon source={icons.home} focused={focused} />
           ),
         }}
       />
-      {/*  <Tabs.Screen*/}
-      {/*    name="rides"*/}
-      {/*    options={{*/}
-      {/*      title: "Rides",*/}
-      {/*      headerShown: false,*/}
-      {/*      tabBarIcon: ({ focused }) => (*/}
-      {/*        <TabIcon source={icons.list} focused={focused} />*/}
-      {/*      ),*/}
-      {/*    }}*/}
-      {/*  />*/}
-      {/*  <Tabs.Screen*/}
-      {/*    name="chat"*/}
-      {/*    options={{*/}
-      {/*      title: "Chat",*/}
-      {/*      headerShown: false,*/}
-      {/*      tabBarIcon: ({ focused }) => (*/}
-      {/*        <TabIcon source={icons.chat} focused={focused} />*/}
-      {/*      ),*/}
-      {/*    }}*/}
-      {/*  />*/}
-      {/*  <Tabs.Screen*/}
-      {/*    name="profile"*/}
-      {/*    options={{*/}
-      {/*      title: "Profile",*/}
-      {/*      headerShown: false,*/}
-      {/*      tabBarIcon: ({ focused }) => (*/}
-      {/*        <TabIcon source={icons.profile} focused={focused} />*/}
-      {/*      ),*/}
-      {/*    }}*/}
-      {/*  />*/}
+      <Tabs.Screen
+        name="rides"
+        options={{
+          title: "Rides",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.list} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.chat} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon source={icons.profile} focused={focused} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
