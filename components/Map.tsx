1e4d6cc260f88b9e12a473a21d6000ab02e57d25
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-21 02:33:18 +0530

    add api key

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,16 @@
 import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 const Map = () => {
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+    >
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 };
 
