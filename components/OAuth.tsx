497b31c08f16fb9de5ed1781061546ee29266fc6
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-23 23:56:13 +0530

    finish

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -7,18 +7,18 @@ import { icons } from "@/constants";
 // import { googleOAuth } from "@/lib/auth";
 
 const OAuth = () => {
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  // const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
 
-  const handleGoogleSignIn = async () => {
-    const result = await googleOAuth(startOAuthFlow);
+  // const handleGoogleSignIn = async () => {
+  // const result = await googleOAuth(startOAuthFlow);
 
-    if (result.code === "session_exists") {
-      Alert.alert("Success", "Session exists. Redirecting to home screen.");
-      router.replace("/(root)/(tabs)/home");
-    }
-
-    Alert.alert(result.success ? "Success" : "Error", result.message);
-  };
+  //   if (result.code === "session_exists") {
+  //     Alert.alert("Success", "Session exists. Redirecting to home screen.");
+  //     router.replace("/(root)/(tabs)/home");
+  //   }
+  //
+  //   Alert.alert(result.success ? "Success" : "Error", result.message);
+  // };
 
   return (
     <View>
