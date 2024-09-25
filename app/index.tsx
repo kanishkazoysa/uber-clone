75152add9d05e0fcf0b1ae71ef396563f8489f7c
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-25 23:53:53 +0530

    edit

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,7 @@ import { useAuth } from "@clerk/clerk-expo";
 const Home = () => {
   const { isSignedIn } = useAuth();
 
-  if (isSignedIn) {
-    return <Redirect href={"/(root)/(tabs)/home"} />;
-  }
+ 
   return <Redirect href="/(auth)/welcome" />;
 };
 
