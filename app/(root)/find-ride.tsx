0ac6d7ffdb429f3a10c2890a1c981a1c0f982519
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-10-02 19:10:07 +0530

    edit

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from 'react-native';
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import GoogleTextInput from "@/components/GoogleTextInput";
+// import RideLayout from "@/components/RideLayout";
+import { icons } from "@/constants";
+import { useLocationStore } from "@/store";
 
 const FindRide = () => {
-    return (
-        <View>
-            <Text>Find Ride</Text>
-        </View>
-    )
-}
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
+
+  return (
+    <View >
+     <Text>Find Ride</Text>
+    </View>
+  );
+};
 
 export default FindRide;
